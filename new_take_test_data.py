91102ed1dd76b395beaaa2be0d27491efb030481
Sean Griffin <sean.c.griffin@gmail.com>
2021-08-30 16:06:57 -0500

    Added logging and delays in slow control.

--- new_take_test_data.py
+++ new_take_test_data.py
@@ -3,6 +3,9 @@
 import sys, time
 import serial
 import threading
+import logging 
+
+logger = logging.getLogger()
 
 from pywub import wubctl
 
@@ -10,27 +13,27 @@ def main(port, baudrate, npulses, outputfile):
     
     wub = wubctl.wuBaseCtl(port, baudrate, timeout=0.5)
     uid = wub.UID
-    print(f"wuBase UID: {uid}")
-    print(f"wuBase status: {wub.cmd_status()}")
+    logger.info(f"wuBase UID: {uid}")
+    logger.info(f"wuBase status: {wub.cmd_status()}")
 
     #Grabbed from Chris' code; will want to rework this. 
     sleeptime = 0.1
     runtime = 30
 
     setup_commands = [
-        "status", 
-        "pulser_setup 20000 0.3", 
-        "dac 1 2000", 
-        "fpgaload",
-        "adcconfig",
-        "fpgaload", 
-        "flush_events", 
-        "fpgatrig 0",    
-        "fpgatrig 1",
-        f"pulser_start {npulses}",
+        ["status", 0.1],
+        ["pulser_setup 20000 0.3", 0.1],
+        ["dac 1 2000", 0.1],
+        ["fpgaload", 0.1],         
+        ["adcconfig", 0.1],
+        ["fpgaload", 0.1],
+        ["flush_events", 0.1],
+        ["fpgatrig 0", 0.1],
+        ["fpgatrig 1", 0.1],
+        [f"pulser_start {npulses}", 0.1]
     ]
 
-    results = wub.batch_setup_commands(setup_commands, verbose=True)
+    results = wub.batch_setup_commands(setup_commands)
     
     datafile = None
     if outputfile:
@@ -58,7 +61,8 @@ def main(port, baudrate, npulses, outputfile):
         print(f"{time.ctime(time.time())}  Error: rx thread failed to complete", flush=True)
 
     print(wub.cmd_status())    
-    datafile.close()
+    if datafile is not None: 
+        datafile.close()
     print("Exiting....")
     sys.exit(0)
     
@@ -70,10 +74,26 @@ if __name__ == "__main__":
     parser.add_argument("--baud", type=str, default=115200, help="Baudrate to use during acquisition.")
     parser.add_argument("--ofile", type=str, default=None, help="Output file for test data.")
     parser.add_argument("--npulses", type=int, default=1000, help="Number of test pulses to send.")
+    parser.add_argument("--loglevel", type=str, default="DEBUG", help="Logger level")
     args = parser.parse_args()    
     
-    #print(args)
-    #print(args.ofile)
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.loglevel)
+     
+    logger.setLevel(args.loglevel.upper())
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    #ch.setLevel(args.loglevel.upper())
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)     
+    
+    # add ch to logger
+    logger.addHandler(ch)
+    
+    #exit(0)
     main(args.port, args.baud, args.npulses, args.ofile)
     
 
