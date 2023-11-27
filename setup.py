91620b9956c61f3f9f8d1b8084440c566a1d577f
John Jacobsen <eigenhombre@gmail.com>
2023-11-27 11:03:15 -0600

    Fix python version issue. #93

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
+import sys
 from setuptools import setup
 
+if sys.version_info < (3, 8):
+    extra_deps = ["numpy==1.21"]
+elif sys.version_info[:2] == (3, 8):
+    extra_deps = ["numpy==1.24"]
+else:
+    # Use newer Numpy:
+    extra_deps = ["numpy==1.26.1"]
+
 setup(
-    name='pywub',
-    version='2.1',
-    packages=['pywub'],
-    package_data={'pywub': ['wubase_commands.txt']},
-    install_requires=[
-        'wheel',
-        'coloredlogs>=15.0',
-        'matplotlib',
-        'numpy',
-        'pyserial'
-        ]
+    name="pywub",
+    version="2.1",
+    packages=["pywub"],
+    package_data={"pywub": ["wubase_commands.txt"]},
+    install_requires=["wheel",
+                      "coloredlogs>=15.0",
+                      "matplotlib",
+                      "numpy",
+                      "pyserial"]
+    + extra_deps,
 )
